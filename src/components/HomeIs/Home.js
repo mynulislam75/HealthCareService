903705cf04d5941f56213d5cccb97663b131e501
mynulislam75 <mynulislam181120@gmail.com>
2021-10-23 13:04:07 +0600

    adding second commit

--- src/components/HomeIs/Home.js
+++ src/components/HomeIs/Home.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import './Home.css'
 
 const Home = () => {
+
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('./servicesdata.JSON')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
     return (
         <div>
-            <h3>jhvfjfhbjgjugrtj</h3>
+            <div className="carousel-container container">
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 imges"
+                            src="https://media.istockphoto.com/photos/medical-technology-background-picture-id1255646957"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 h-50 imges"
+                            src="https://media.istockphoto.com/photos/medical-technology-background-picture-id1255646957"
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 h-50 imges"
+                            src="https://media.istockphoto.com/photos/medical-technology-background-picture-id1255646957"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+
+            <div className="services-container row container">
+                {
+                    services.map(service => <div className="singleService col-lg-4">
+
+                            <img src={service.img} alt="" />
+                            <h2>Name:{service.name}</h2>
+                            <h4>Price:{service.price}</h4>
+                            <p>{service.description}</p>
+                            <button>Get This</button>
+                            
+                    </div>)
+                }
+
+            </div>
+
         </div>
     );
 };
