9282bb32b1aa0fb4cafd96347cec00ce6dbbde31
mynulislam75 <mynulislam181120@gmail.com>
2021-10-26 07:33:09 +0600

    modify readme file

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Getting Started with Create React App
 
-### THIS IS THE WEBSITE OF OUR MEDICAL CENTER
+# THIS IS THE WEBSITE OF OUR MEDICAL CENTER
+-By visiting this website one can easily know our services and thus can be helped .
+-The projects link is : https://friendly-franklin-9baf77.netlify.app/
+
 # BY VISITING THIS WEBSITE ONE CAN EASILY GET BENEFITS
+-we have plenty of good services.And this website represents it easily.
+# WHY I MADE THIS WEBSITE
+-I want to know people about ourselves.And also want everyone get proper services .And this website can help them as it represents a plenty of good services
+# HOW CAN BE THIS WEBSITE HELPFUL
+-We added our contacts and others important link.By using this one can easily get our services in his emergency moments
+# Which language I used to make this website?
+-I use HTML,CSS,Bootstrap,React js in this website.
--- src/components/Doctors/Doctors.js
+++ src/components/Doctors/Doctors.js
@@ -36,7 +36,7 @@ const Doctors = () => {
                 </div>
                 <div className="col-md-6 col-lg-4">
                     <div id="single-doctors" className="p-3 m-2 border">
-                        <h2>AHMED ALI</h2>
+                        <h2>MD ALI</h2>
                         <p>CARDIOLOGIST</p>
                         <small>10AM-12PM</small>
                         <br />
@@ -47,7 +47,7 @@ const Doctors = () => {
                 </div>
                 <div className="col-md-6 col-lg-4">
                     <div id="single-doctors" className="p-3 m-2 border">
-                        <h2>AHMED ALI</h2>
+                        <h2>SAGOR DAS</h2>
                         <p>EYE SPECIALISTS</p>
                         <small>8AM-12PM</small>
                         <br />
@@ -58,7 +58,7 @@ const Doctors = () => {
                 </div>
                 <div className="col-md-6 col-lg-4">
                     <div id="single-doctors" className="p-3 m-2 border">
-                        <h2>AHMED ALI</h2>
+                        <h2>ALI HASAN</h2>
                         <p>NUROLOGIST</p>
                         <small>9AM-12PM</small>
                         <br />
@@ -69,7 +69,7 @@ const Doctors = () => {
                 </div>
                 <div className="col-md-6 col-lg-4">
                     <div id="single-doctors" className="p-3 m-2 border">
-                        <h2>AHMED ALI</h2>
+                        <h2>IFTEHKHAR BIN MONIR</h2>
                         <p>EUROLOGIST</p>
                         <small>10AM-12PM</small>
                         <br />
@@ -80,7 +80,7 @@ const Doctors = () => {
                 </div>
                 <div className="col-md-6 col-lg-4">
                     <div id="single-doctors" className="p-3 m-2 border">
-                        <h2>AHMED ALI</h2>
+                        <h2>MONIRUL ISLAM</h2>
                         <p>ENT</p>
                         <small>10AM-12PM</small>
                         <br />
